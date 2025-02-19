28b61c147100bca9ea9bd31135e895a573c0a644
chenryry <1222bluestar@gmail.com>
2025-02-18 20:13:49 -0500

    added basic chatbot using rag

---
+++ chatbot.py
@@ -0,0 +1,40 @@
+import langchain
+import numpy as np
+import pandas as pd
+import re
+import json
+from tqdm import tqdm
+import torch
+import gc
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from summary import extracted_summaries
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+vector_db = FAISS.from_texts(extracted_summaries, embedding_model)
+
+llm_pipeline = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=50,  
+    temperature=0.1,  
+    do_sample=False  
+)   
+phi_llm = HuggingFacePipeline(pipeline=llm_pipeline)
+
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm=phi_llm,
+    retriever=vector_db.as_retriever(),
+    return_source_documents=True  
+)
+
+def chat_with_bot(query):
+    response = qa_chain.invoke({"query": query})  
+    return response["result"]  
+
+query = "What are the main causes for appeals in cases?"
+response = chat_with_bot(query)
+print("Chatbot Answer:", response)
